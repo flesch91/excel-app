d1951574e616de25cc5aba57709814ef79f26eac
nazariibanakh <banakh.nazarii@gmail.com>
2020-06-28 12:45:21 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+import path from "path";
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: "development",
+  entry: './index.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  }
+};
